c494071d4d759797b60a9746ff914a760a71cecc
Diwanga <diwangaamasith@gmai.com>
2020-06-03 23:32:05 +0530

    Updates

---
+++ precache-manifest.0acd98d182261d294970825b186374cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee277ad9e316f742e182ce7551cb39a1",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "77991623798ab43ca71e",
+    "url": "/Monster-Rolodex/static/css/main.c9f536d1.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/Monster-Rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77991623798ab43ca71e",
+    "url": "/Monster-Rolodex/static/js/main.7dcfa24b.chunk.js"
+  },
+  {
+    "revision": "39754c773e1d7fb81761",
+    "url": "/Monster-Rolodex/static/js/runtime-main.b0e90f1c.js"
+  }
+]);
